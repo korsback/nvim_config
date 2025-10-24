b359052a72205fdc83a813e9db3cf7b90726804e
Oskar K <oskar.korsback@decerno.se>
2025-10-24 11:30:25 +0200

    update theme

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -22,7 +22,7 @@ return {
     "navarasu/onedark.nvim",
     config = function()
       require('onedark').setup {
-        style = 'darker'
+        style = 'deep'
       }
       require('onedark').load()
     end
