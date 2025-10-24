95d21c232bbde84e8d475a71556372a58ab3ff37
Oskar K <oskar.korsback@decerno.se>
2025-10-24 12:59:33 +0200

    open telescope on startup, disable lazy update status message

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require("config.lazy")
 require("setup")
 
+-- Open telescope if no fileargument is provided to nvim
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      require("telescope.builtin").find_files()
+    end
+  end,
+})
