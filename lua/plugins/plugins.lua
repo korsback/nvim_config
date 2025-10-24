faa9cbd8bd4e59348ad37f2f65d07dd01278df4f
Oskar K <oskar.korsback@decerno.se>
2025-10-24 10:45:19 +0200

    add open buffer telescope

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -21,6 +21,7 @@ return {
     keys = {
       { '<leader>ff', function() require('telescope.builtin').find_files() end, mode = 'n', desc = 'Find files' },
       { '<leader>fg', function() require('telescope.builtin').live_grep() end, mode = 'n', desc = 'Live grep' },
+      { '<leader>fb', function() require('telescope.builtin').buffers() end, mode = 'n', desc = 'Live grep' },
     }
   },
   {
